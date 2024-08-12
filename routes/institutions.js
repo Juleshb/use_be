3a0feaf6ace2364e66afc716b0da29748a468de6
juleshb <habaruremajules@gmail.com>
2024-08-12 15:33:33 +0200

    updated data base

--- routes/institutions.js
+++ routes/institutions.js
@@ -4,8 +4,8 @@ const institutionsController = require('../controllers/institutionsController');
 const auth = require('../middleware/auth');
 
 // Protected routes with role-based access
-router.get('/', auth(), institutionsController.getAllInstitutions); // Any authenticated user can access
-router.get('/:id', auth(), institutionsController.getInstitutionById); // Any authenticated user can access
+router.get('/', institutionsController.getAllInstitutions); // Any authenticated user can access
+router.get('/:id', institutionsController.getInstitutionById); // Any authenticated user can access
 router.post('/', auth('admin'), institutionsController.createInstitution); // Only admins can create institutions
 router.put('/:id', auth('admin'), institutionsController.updateInstitution); // Only admins can update institutions
 router.delete('/:id', auth('super-admin'), institutionsController.deleteInstitution); // Only super-admins can delete institutions
